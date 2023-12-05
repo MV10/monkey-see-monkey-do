cac25d48bcf3f98eca81ee362a0d9e0c178cb983
Jon McGuire <mcguirev10@gmail.com>
2023-12-05 11:07:56 -0500

    more BOM stupidity, and use CWD to create service

--- msmd/msmd/WinServiceCreate.cmd
+++ msmd/msmd/WinServiceCreate.cmd
@@ -1,6 +1,6 @@
 @openfiles.exe 1>nul 2>&1
 @if not %errorlevel% equ 0 goto :fail
-sc create "Monkey Hi Hat TCP Relay (msmd)" binpath= "C:\Program Files\mhh\msmdwindows.exe" start= delayed-auto displayname= "McGuireV10.com Monkey Hi Hat Remote Command Relay"
+sc create "Monkey Hi Hat TCP Relay (msmd)" binpath= "%~dp0msmd.exe" start= delayed-auto displayname= "McGuireV10.com Monkey Hi Hat Remote Command Relay"
 @goto:eof
 :fail
 @echo:
